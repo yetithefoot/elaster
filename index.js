992582e4bbba009a86b2ed9b8f77f9473676b912
Vlad Tsepelev <reg.yeti@gmail.com>
2017-10-06 18:16:15 +0300

    fix elaster

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-var _ = require('underscore');
-var moment = require('moment');
-var async = require('async');
-var through = require('through');
-var single = require('single-line-log');
-var highland = require('highland');
-var elasticsearch = require('elasticsearch');
-var mongojs = require('mongojs');
-var db, elastic;
-var options = {
-	simultaneousOperations: 1000
-};
-
 require('colors');
+const moment = require('moment');
+const async = require('async');
+const through = require('through');
+const single = require('single-line-log');
+const highland = require('highland');
+const elasticsearch = require('elasticsearch');
+const mongojs = require('mongojs');
+const defaults = require('lodash.defaults');
+const pick = require('lodash.pick');
+let db, elastic;
+
 
 function format(duration) {
 	return duration.hours() + ':' + duration.minutes() + ':' + duration.seconds() + ':' + duration.milliseconds();
 }
 
-function exportCollection(desc, settings, options={}, callback) {
+function exportCollection(desc, settings, options = { }, callback) {
 
-	var collection = db[desc.name];
-	var query = desc.query || {};
+	options = defaults(options, { batchSize: 1000 });
+
+	const collection = db[desc.name];
+	const query = desc.query || {};
 
 	if (!collection) {
 		return callback('collection ' + desc.name + ' does not exist.');
@@ -102,9 +102,7 @@ function exportCollection(desc, settings, options={}, callback) {
 			console.log('----> open index connection[' + desc.index + ']');
 			elastic.indices.open(
 				{index: desc.index},
-				err => {
-					next(err);
-				}
+				err => next(err)
 			);
 		},
 		next => {
@@ -123,21 +121,21 @@ function exportCollection(desc, settings, options={}, callback) {
 		(total, next) => {
 			console.log('----> streaming collection to elastic');
 			if(desc.preformers) {
-				console.log(`-----> preformers run for fields: ${_.keys(desc.preformers)}`);
+				console.log(`-----> preformers run for fields: ${Object.keys(desc.preformers)}`);
 			}
-			var takeFields = through(function(items) {
+			const takeFields = through(function(items) {
 
-				this.queue(_.map(items, item => {
+				this.queue(items.map(item => {
 					if (desc.fields) {
-						item = _.pick(item, desc.fields);
+						item = pick(item, desc.fields);
 					}
 
 					// if there is preformers (pre index transformer) for field run it
 					// cases: sometimes before indexing we need to transform initial value somehow
 					if(desc.preformers) {
-						for(var field in desc.preformers){
+						for(let field in desc.preformers){
 							if(item[field]){
-								var fn = desc.preformers[field];
+								const fn = desc.preformers[field];
 								item[field] = fn(item[field]);
 							}
 						}
@@ -146,11 +144,10 @@ function exportCollection(desc, settings, options={}, callback) {
 				}));
 			});
 
-			var postToElastic = through(function (item) {
-				// console.log(item);
-				var me = this;
-				var bulkBody = [];
-				me.pause();
+			const postToElastic = through(function (item) {
+				
+				const bulkBody = [];
+				this.pause();
 
 				for (i = 0; i < item.length; i++){
 					bulkBody.push({
@@ -163,14 +160,10 @@ function exportCollection(desc, settings, options={}, callback) {
 					delete item[i]._id;
 					bulkBody.push(item[i]);
 				}
-				//console.log(item.length);
-				//console.dir(bulkBody);
-				// me.queue(item.length);
-				// me.resume();
 
 				elastic.bulk({
 					body: bulkBody
-				}, function (err, resp) {
+				}, (err, resp) => {
 					if (err) {
 						//console.error(('response timeout or failed to connect.').bold.red);
 						console.dir(err);
@@ -181,24 +174,24 @@ function exportCollection(desc, settings, options={}, callback) {
 						return next(resp);
 					}
 
-					me.queue(item.length);
-					me.resume();
+					this.queue(item.length);
+					this.resume();
 				});
 			});
 
-			var progress = function () {
-				var count = 0;
+			const progress = function () {
+				let count = 0;
 				return through(function (length) {
 					count += length;
-					var percentage = Math.floor(100 * count / total);
+					const percentage = Math.floor(100 * count / total);
 					single(('------> processed ' + count + ' documents [' + percentage + '%]').magenta);
 				});
 			};
 
-			var cursor = collection.find(query).sort({_id: 1});
+			const cursor = collection.find(query).sort({_id: 1});
 
-			var stream = highland(cursor)
-				.batch(options.simultaneousOperations)
+			const stream = highland(cursor)
+				.batch(options.batchSize || 1000)
 				.pipe(takeFields)
 				.pipe(postToElastic)
 				.pipe(progress());
@@ -236,11 +229,11 @@ function exportCollection(desc, settings, options={}, callback) {
 	], function (err) {
 		if (err) {
 			console.error(('====> collection [' + desc.name + '] - failed to export.\n').bold.red);
-			console.error(err);
+			console.error(JSON.stringify(err,null,2));
 			return callback(err);
 		}
 
-		var duration = moment.duration(moment().diff(started));
+		const duration = moment.duration(moment().diff(started));
 
 		console.log(('====> collection [' + desc.name + '] - exported successfully.').green);
 		console.log(('====> time elapsed ' + format(duration) + '\n').green);
@@ -257,21 +250,16 @@ function close() {
 	}
 }
 
-function exporter(config) {
+function run(config) {
 	db = mongojs(config.mongo.connection);
 	elastic = elasticsearch.Client(config.elastic);
 
 	// export all collections
-	var collections = config.collections;
-	var exports = collections.map(function (c) {
-		return function (callback) {
-			exportCollection(c, config.settings, config.options, callback);
-		};
-	});
-
+	const collections = config.collections;
+	const exports = collections.map(c => callback => exportCollection(c, config.settings, config.options, callback));
 	async.series(exports, close);
 }
 
 module.exports = {
-	run: exporter
+	run
 };
